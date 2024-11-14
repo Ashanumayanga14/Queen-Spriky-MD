ae82d372a3619e3c7bed51b017148392983fb674
Ashanumayanga14 <165387571+Ashanumayanga14@users.noreply.github.com>
2024-11-14 15:32:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
-    Owner: process.env.OwnerNumber || '94758900210',    // Enter Your Owner Number
+    SESSION_ID: process.env.SESSION_ID || 'j2xHwIKQ#_QwwD1y1skrvkp4xrErrW9K9JJxLMZNkMf7Q3zm7zjc', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || 'mongodb://mongo:eAMADLypGVQPgLcWRGBiYmWolbYgPTKj@autorack.proxy.rlwy.net:28696',// Enter Your MongoDB URL
+    Owner: process.env.OwnerNumber || '94768725104',    // Enter Your Owner Number
 };
